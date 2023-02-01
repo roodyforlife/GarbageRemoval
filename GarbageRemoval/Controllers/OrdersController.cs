4a28fc9b77a14e8933f437e1233784e3d377508a
Колосс Матвій <kolossmatveyka@gmail.com>
2023-02-01 21:54:42 +0200

    add filtration

--- GarbageRemoval/Controllers/OrdersController.cs
+++ GarbageRemoval/Controllers/OrdersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GarbageRemoval.DataBase;
 using GarbageRemoval.Models;
+using GarbageRemoval.Enums;
 
 namespace GarbageRemoval.Controllers
 {
@@ -20,9 +21,88 @@ namespace GarbageRemoval.Controllers
         }
 
         // GET: Orders
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string bridageName, string address, DateTime createDateFrom, DateTime createDateTo, 
+            int costFrom, int costTo, OrderSortState sort = OrderSortState.BrigadeNameAsc)
         {
-            var applicationContext = _context.Orders.Include(o => o.Brigade).Include(o => o.House);
+            IQueryable<Order> applicationContext = _context.Orders.Include(o => o.Brigade).Include(o => o.House);
+
+            if (bridageName is not null)
+            {
+                applicationContext = applicationContext.Where(x => x.Brigade.BrigadeName.Contains(bridageName));
+            }
+
+            if (address is not null)
+            {
+                applicationContext = applicationContext.Where(x => x.House.Address.Contains(address));
+            }
+
+            if (createDateTo.Year != 1)
+            {
+                applicationContext = applicationContext.Where(x => x.CreateDate <= createDateTo);
+            }
+
+            applicationContext = applicationContext.Where(x => x.CreateDate >= createDateFrom);
+
+            if (costTo != 0)
+            {
+                applicationContext = applicationContext.Where(x => x.CostPerKilogram <= costTo);
+            }
+
+            applicationContext = applicationContext.Where(x => x.CostPerKilogram >= costFrom);
+
+            switch (sort)
+            {
+                case OrderSortState.BrigadeNameAsc:
+                    applicationContext = applicationContext.OrderBy(x => x.Brigade.BrigadeName);
+                    break;
+                case OrderSortState.BrigadeNameDesc:
+                    applicationContext = applicationContext.OrderByDescending(x => x.Brigade.BrigadeName);
+                    break;
+                case OrderSortState.AddressAsc:
+                    applicationContext = applicationContext.OrderBy(x => x.House.Address);
+                    break;
+                case OrderSortState.AddressDesc:
+                    applicationContext = applicationContext.OrderByDescending(x => x.House.Address);
+                    break;
+                case OrderSortState.IsServedAsc:
+                    applicationContext = applicationContext.OrderBy(x => x.IsServed);
+                    break;
+                case OrderSortState.IsServedDesc:
+                    applicationContext = applicationContext.OrderByDescending(x => x.IsServed);
+                    break;
+                case OrderSortState.CostAsc:
+                    applicationContext = applicationContext.OrderBy(x => x.CostPerKilogram);
+                    break;
+                case OrderSortState.CostDesc:
+                    applicationContext = applicationContext.OrderByDescending(x => x.CostPerKilogram);
+                    break;
+                case OrderSortState.WeightAsc:
+                    applicationContext = applicationContext.OrderBy(x => x.GarbadgeWeight);
+                    break;
+                case OrderSortState.WeightDesc:
+                    applicationContext = applicationContext.OrderByDescending(x => x.GarbadgeWeight);
+                    break;
+                default:
+                    applicationContext = applicationContext.OrderBy(x => x.Brigade.BrigadeName);
+                    break;
+            }
+
+            ViewBag.Sort = (List<SelectListItem>)Enum.GetValues(typeof(OrderSortState)).Cast<OrderSortState>()
+               .Select(x => new SelectListItem
+               {
+                   Text = x.ToString(),
+                   Value = x.ToString(),
+                   Selected = (x == sort)
+               }).ToList();
+
+            ViewBag.BrigadeName = bridageName;
+            ViewBag.Address = address;
+            ViewBag.CreateDateFrom = createDateFrom;
+            ViewBag.CreateDateTo = createDateTo;
+            ViewBag.CostFrom = costFrom;
+            ViewBag.CostTo = costTo;
+
+
             return View(await applicationContext.ToListAsync());
         }
 
---
+++ GarbageRemoval/Enums/OrderSortState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarbageRemoval.Enums
+{
+    public enum OrderSortState
+    {
+        BrigadeNameAsc,
+        BrigadeNameDesc,
+        AddressAsc,
+        AddressDesc,
+        IsServedAsc,
+        IsServedDesc,
+        CostAsc,
+        CostDesc,
+        WeightAsc,
+        WeightDesc
+    }
+}
